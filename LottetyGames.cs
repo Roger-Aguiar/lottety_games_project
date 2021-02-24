14992b56beb78cad442f5836280e67b37eef4d66
Roger <roger.silvaaguiar@yahoo.com.br>
2021-02-24 19:48:01 -0300

    My first commit: Creating the project.

---
+++ LottetyGames.cs
@@ -0,0 +1,2 @@
+//Name:         Roger Silva Santos Aguiar
+//Initial date: February 24, 2021
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LottetyGamesProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
