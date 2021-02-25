f6769a5cdfc10f14d1f0c3d1d6bee0dda989f1d2
Roger <roger.silvaaguiar@yahoo.com.br>
2021-02-25 11:23:10 -0300

    GenerateGame function was implemented and some updates were done in the LotteryGames class.

--- LotteryGames.cs
+++ LotteryGames.cs
@@ -1,22 +1,26 @@
 //Name:         Roger Silva Santos Aguiar
 //Initial date: February 24, 2021
 
+using System.Collections.Generic;
 public abstract class LotteryGames
 {
     private int quantityOfAvailableNumbers;
     private int quantiyOfNumbersByGame;
     private string fileWithResults;
-    private string [] game;
-    private string [] pastResults;
+    private List<string> available_numbers = new List<string>();
+    private List<string> game = new List<string>();
+    private List<string> pastResults = new List<string>();
+    List<int> frequency_of_numbers = new List<int>();
 
     public int QuantityOfAvailableNumbers { get => quantityOfAvailableNumbers; set => quantityOfAvailableNumbers = value; }
     public int QuantiyOfNumbersByGame { get => quantiyOfNumbersByGame; set => quantiyOfNumbersByGame = value; }
-    public string FileWithResults { get => fileWithResults; set => fileWithResults = value; }
-    public string[] Game { get => game; set => game = value; }
-    public string[] PastResults { get => pastResults; set => pastResults = value; }
-    
+    public string FileWithResults { get => fileWithResults; set => fileWithResults = value; }   
+    public List<string> PastResults { get => pastResults; set => pastResults = value; }
+    public List<string> Game { get => game; set => game = value; }
+    public List<string> Available_numbers { get => available_numbers; set => available_numbers = value; }
+    public List<int> Frequency_of_numbers { get => frequency_of_numbers; set => frequency_of_numbers = value; }
 
-    public abstract string[] GenerateGames();
+    public abstract void GenerateGames();
     public abstract string GetLayoutGame();
     public abstract void GetPastResults();
 
